10a6f8eb0a86ba77cbed19a3470424631cf01872
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-07 16:23:40 +0200

    test ejercicio 1

---
+++ test_ejercicio1.py
@@ -0,0 +1,12 @@
+import unittest
+from ejercicios.ejercicio1 import Alumno
+class TestEjercicio1(unittest.TestCase):
+
+    def test_alumno(self):
+        alumno1 = Alumno("Juan", "Pérez", 7)
+        alumno2 = Alumno("Maria", "García", 4)
+        self.assertEqual(alumno1.calificacion(), "Aprobado")
+        self.assertEqual(alumno2.calificacion(), "Suspenso")
+
+if __name__ == '__main__':
+    unittest.main()
