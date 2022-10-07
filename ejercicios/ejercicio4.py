455858c5810032dbcfd9bf949788f1a6ed01595b
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-07 15:53:18 +0200

    hecha parte 2 ejercicio 4

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -3,3 +3,10 @@ try:
     resultado = 7/0
 except ZeroDivisionError:
     print("Error: No es posible dividir entre cero, introduzca un número distinto.")
+
+lista = [4, 7, 30, 23, 5]
+
+try:
+    lista[10]
+except IndexError:
+    print("Error: El índice al que intenta usted acceder se encuentra fuera del rango que abarca la lista, introduzca un número mayor o igual que cero y menor que la longitud de la lista.")
