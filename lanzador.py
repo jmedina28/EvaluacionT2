d6fa131b74b324cc77bf8a201015a9f512cf2898
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-07 16:47:51 +0200

    modificaciones finales

--- lanzador.py
+++ lanzador.py
@@ -2,34 +2,35 @@ from introducir import solicitar_introducir_numero
 from introducir.numero import solicitar_introducir_numero_extremo
 
 
-seleccion = solicitar_introducir_numero_extremo("Introduzca el número del ejercicio que desea ejecutar: ", 1, 5)
-
-if seleccion == 1:
-    from ejercicios.ejercicio1 import Alumno
-    alumno = Alumno("Juan", "Pérez", 7)
-    print(alumno.calificacion())
-    alumno2 = Alumno("Maria", "García", 4)
-    print(alumno2.calificacion())
-
-elif seleccion == 2:
-    from ejercicios.ejercicio2 import Alumno
-    alumno = Alumno("Juan", "Pérez", 7)
-    print(alumno)
-    print(alumno.calificacion())
-    alumno2 = Alumno("Maria", "García", 4)
-    print(alumno2)
-    print(alumno2.calificacion())
+def ejecutar():
     
-elif seleccion == 3:
-    from ejercicios.ejercicio3 import Producto
+    seleccion = solicitar_introducir_numero_extremo("Introduzca el número del ejercicio que desea ejecutar: ", 1, 5)
+    if seleccion == 1:
+        from ejercicios.ejercicio1 import Alumno
+        alumno = Alumno("Juan", "Pérez", 7)
+        print(alumno.calificacion())
+        alumno2 = Alumno("Maria", "García", 4)
+        print(alumno2.calificacion())
 
-    producto = Producto("0001","Producto 1","Descripción del producto 1",100)
-    print(producto)
-    producto.pvp = 200
-    print(""),print(producto)
+    elif seleccion == 2:
+        from ejercicios.ejercicio2 import Alumno
+        alumno = Alumno("Juan", "Pérez", 7)
+        print(alumno)
+        print(alumno.calificacion())
+        alumno2 = Alumno("Maria", "García", 4)
+        print(alumno2)
+        print(alumno2.calificacion())
+        
+    elif seleccion == 3:
+        from ejercicios.ejercicio3 import Producto
 
-elif seleccion == 4:
-    import ejercicios.ejercicio4
+        producto = Producto("0001","Producto 1","Descripción del producto 1",100)
+        print(producto)
+        producto.pvp = 200
+        print(""),print(producto)
 
-elif seleccion == 5:
-    from ejercicios import ejercicio5
+    elif seleccion == 4:
+        import ejercicios.ejercicio4
+
+    elif seleccion == 5:
+        import ejercicios.ejercicio5
---
+++ main.py
@@ -0,0 +1,4 @@
+import lanzador
+
+if __name__ == "__main__":
+    lanzador.ejecutar()
