94a1cc5271967c46c72c3636b5e3720c2f031643
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-07 16:01:05 +0200

    añadida estructura incial del ejercicio5

---
+++ ejercicios/ejercicio5.py
@@ -0,0 +1,19 @@
+class Vehiculo():
+    
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+        
+    def __str__(self):
+        return "color {}, {} ruedas".format( self.color, self.ruedas )
+        
+        
+class Coche(Vehiculo):
+    
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+        
+    def __str__(self):
+        return super().__str__() + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
