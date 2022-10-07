5bef26245770fbb23deb76162a76fe45950adcab
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-07 16:04:14 +0200

    hecha la camioneta

--- ejercicios/ejercicio5.py
+++ ejercicios/ejercicio5.py
@@ -20,3 +20,15 @@ class Coche(Vehiculo):
 
 coche = Coche("azul", 4, 150, 1300)
 print(coche)
+
+class Camioneta(Coche):
+    
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+        
+    def __str__(self):
+        return super().__str__() + ", soporta {} kg de carga".format(self.carga)
+
+camioneta = Camioneta("verde", 4, 90, 2000, 750)
+print(camioneta)
